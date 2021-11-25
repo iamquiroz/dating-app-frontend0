7cee1e95c1fe838edf02863dcd0c6095d14e9b5f
Josh W <jquiroz.mexicali@gmail.com>
2021-11-24 21:20:58 -0800

    breakpoint

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import firebaseConfig from './firebaseConfig';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+
+render(
+ ( <FirebaseAppProvider firebaseConfig={ firebaseConfig }>
+  <App />
+  </FirebaseAppProvider>
+ )
   document.getElementById("root")
 );
